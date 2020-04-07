e01c1103726e4d3fffff308d6f06a0f1e7ea0bbb
Ethan Halsall <ethan.halsall@gmail.com>
2020-04-07 22:37:46 +0100

    sched: remove some unused bits
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -111,8 +111,3 @@ SCHED_FEAT(FIND_BEST_TARGET, false)
  *   those tasks through the mainline slow path.
  */
 SCHED_FEAT(EAS_PREFER_IDLE, true)
-
-/*
- * Request max frequency from schedutil whenever a RT task is running.
- */
-SCHED_FEAT(SUGOV_RT_MAX_FREQ, false)
